ce3bae630fd64bd39c7be532d65fd11b2ad0be8f
e.unurbayar83@gmail.com <e.unurbayar83@gmail.com>
2022-06-25 14:46:29 -0500

    add

---
+++ js/index.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+
+    let myAudio1 = document.getElementById('player1');
+    let play = document.getElementById('play');
+    let pause = document.getElementById('pause');
+  
+    // associate functions with the 'onclick' events
+    play.onclick = playAudio1;
+    pause.onclick = pauseAudio1;
+  
+    function playAudio1() {
+      myAudio1.play();
+    }
+  
+    function pauseAudio1() {
+      myAudio1.pause();
+    }
+  }
+  
