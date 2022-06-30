e03975b559b8602ebed106b5771b7c3849bff60b
e.unurbayar83@gmail.com <e.unurbayar83@gmail.com>
2022-06-29 22:12:58 -0500

    change

--- index.js
+++ index.js
@@ -14,128 +14,60 @@ const audio1 = document.getElementById('audio1');
 
 
 
+
 const musicContainer2 = document.getElementById('music-container2');
 const playBtn2 = document.getElementById('play2');
 const prevBtn2 = document.getElementById('prev2');
 const nextBtn2 = document.getElementById('next2');
 
 const audio2 = document.getElementById('audio2');
-const title2 = document.getElementById('title2');
-const cover2 = document.getElementById('cover2');
+
 
 
 // Song titles
-const songs = ['hey', 'summer', 'ukulele'];
+const songs1 = ['hey', 'summer', 'ukulele'];
+const songs2 = ['summer', 'hey', 'ukulele'];
+
 
 // Keep track of song
-let songIndex = 2;
+let songIndex1 = 2;
+let songIdnex2 = 1;
 
 // Initially load song details into DOM
-loadSong(songs[songIndex]);
+loadSong(songs1[songIndex1], songs2[songIdnex2]);
 
 // Update song details
-function loadSong(song) {
-//   title.innerText = song;
-  audio1.src = `music/${song}.mp3`;
-  audio2.src = `music/${song}.mp3`;
+function loadSong(song1, song2) {
+  
+  audio1.src = `music/${song1}.mp3`;
+  audio2.src = `music/${song2}.mp3`;
 }
 
 // Play song
-function playSong() {
-  musicContainer1.classList.add('play1');
-  playBtn1.querySelector('i.fas').classList.remove('fa-play');
-  playBtn1.querySelector('i.fas').classList.add('fa-pause');
+function playSong(playerId) {
+  let player = document.getElementById(playerId)
+  let audio = player.querySelector('audio');
+  let playBtn = player.querySelector('.play-btn');
+  
+  player.classList.add(player.find);
+  console.log("clicked: " + playerId)
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
 
-  musicContainer2.classList.add('play2');
-//   playBtn2.querySelector('i.#p2').classList.remove('fa-play');
-//   playBtn2.querySelector('i.#p2').classList.add('fa-pause');
 
-  audio1.play();
-
-  audio2.play();
+  audio.play();
+ 
 }
 
 // Pause song
-function pauseSong() {
-  musicContainer1.classList.remove('play');
-  playBtn1.querySelector('i.fas').classList.add('fa-play');
-  playBtn1.querySelector('i.fas').classList.remove('fa-pause');
+function pauseSong(playerId) {
 
-  audio1.pause();
+  let player = document.getElementById(playerId)
+  let audio = player.querySelector('audio');
+  let puseBtn = player.querySelector('fa-pause');
 
-  musicContainer2.classList.remove('play');
-  playBtn2.querySelector('i.#p2').classList.add('fa-play');
-  playBtn2.querySelector('i.#p2').classList.remove('fa-pause');
 
-  audio2.pause();
-}
 
-// Previous song
-function prevSong() {
-  songIndex--;
-
-  if (songIndex < 0) {
-    songIndex = songs.length - 1;
-  }
-
-  loadSong(songs[songIndex]);
-
-  playSong();
-}
-
-// Next song
-function nextSong() {
-  songIndex++;
-
-  if (songIndex > songs.length - 1) {
-    songIndex = 0;
-  }
-
-  loadSong(songs[songIndex]);
-
-  playSong();
-}
-
-// Event listeners
-playBtn1.addEventListener('click', () => {
-  const isPlaying1 = musicContainer1.classList.contains('play1');
-
-  if (isPlaying1) {
-    pauseSong();
-  } else {
-    playSong();
-  }
-
-  const isPlaying2 = musicContainer1.classList.contains('play2');
-
-  if (isPlaying2) {
-    pauseSong();
-  } else {
-    playSong();
-  }
-
-});
-
-// playBtn2.addEventListener('click', () => {
-// 	const isPlaying2 = musicContainer2.classList.contains('play2');
-  
-// 	if (isPlaying2) {
-// 	  pauseSong();
-// 	} else {
-// 	  playSong();
-// 	}
-  
-  
-//   });
-
-// Change song
-prevBtn1.addEventListener('click', prevSong);
-nextBtn1.addEventListener('click', nextSong);
-
-audio1.addEventListener('ended', nextSong);
-
-// Pause song
-function pauseSong() {
   musicContainer1.classList.remove('play1');
   playBtn1.querySelector('i.fas').classList.add('fa-play');
   playBtn1.querySelector('i.fas').classList.remove('fa-pause');
@@ -205,6 +137,5 @@ prevBtn1.addEventListener('click', prevSong);
 nextBtn1.addEventListener('click', nextSong);
 
 audio1.addEventListener('ended', nextSong);
-
 
 
